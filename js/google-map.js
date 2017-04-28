08b00cb21ce8773be60a3310bb0a04ebc507172f
pscroce <pscroce@gmail.com>
2017-04-28 16:53:11 -0400

    Aquatic Gems Page layout built out and all map and social media data added. Ready for Weebly design flow

--- js/google-map.js
+++ js/google-map.js
@@ -2,10 +2,18 @@ function initGoogleMap()
 {
   infoWrittenHeight = jQuery('#expanded-info__written').outerHeight();
   infoMap = jQuery('#expanded-info__map');
+  viewportHeight = jQuery(window).height();
   viewportWidth = jQuery(window).width();
+  smartphoneWidth = 421;
 
-  // Size the map according to the size of the area to its left
-  jQuery(infoMap).css("height", infoWrittenHeight +"px");
+  // If the viewport is not a smartphone,
+  // size the map according to the size of the area to its left
+  if(viewportWidth >= smartphoneWidth) {
+    infoWrittenHeight = jQuery('#expanded-info__written').outerHeight();
+    jQuery(infoMap).css("height", infoWrittenHeight +"px");
+  } else {
+    jQuery(infoMap).css("height", viewportHeight +"px");
+  }
 
   // Add the '.clicked' class if map is clicked, remove it if mouse leaves
   // Nice solution from: https://codepen.io/status201/pen/wKowKz
@@ -19,9 +27,11 @@ function initGoogleMap()
   {
     // If the viewport is not a smartphone, resize the map according to the
     // size of the area to its left when the window is resized.
-    if(viewportWidth >= 421) {
+    if(viewportWidth >= smartphoneWidth) {
       infoWrittenHeight = jQuery('#expanded-info__written').outerHeight();
       jQuery(infoMap).css("height", infoWrittenHeight +"px");
+    } else {
+      jQuery(infoMap).css("height", viewportHeight +"px");
     }
 
   });
