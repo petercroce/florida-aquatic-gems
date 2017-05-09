fc560e7fbfabe652506b2e7570fa53c5d2b72521
pscroce <pscroce@gmail.com>
2017-05-09 14:17:52 -0400

    Changes to nav, icons, style tweaks. Add button around nav on desktop. Add bouncing chevron down to indicate page scroll

--- js/fullscreen-menu.js
+++ js/fullscreen-menu.js
@@ -1,5 +1,21 @@
 function initFullscreenMenu()
 {
+  // function menuResize() {
+  //   var menu = jQuery(".wsite-menu-default");
+  //   var menuHeight = menu.height();
+  //   var menuItem = jQuery(".wsite-menu-item-wrap");
+  //   var documentHeight = jQuery(document).innerHeight();
+  //   var documentWidth = jQuery(document).innerWidth();
+  //   var tabletWidth = 992;
+  //   if ((menuHeight > (documentHeight)/2) && (documentWidth > tabletWidth)) {
+  //     menu.css("font-size", "1.2rem");
+  //     menuItem.css("width", "25%");
+  //   }
+  // }
+  // menuResize();
+  // jQuery(window).resize(function(){
+  //   menuResize();
+  // })
 
   jQuery('.hamburger').on('click', function(){
     // Prevent jump
@@ -10,8 +26,24 @@ function initFullscreenMenu()
     jQuery('body').toggleClass('nav-open')
       .find('.hamburger').toggleClass('is-active');
   });
+  jQuery('.home-page .wsite-button').on('click', function(){
+    // Prevent jump
+    if ( jQuery(window).height() < jQuery('html').height() ) {
+      jQuery('html').addClass('scrollbar');
+    }
+    // Add the classes to open nav and change hamburger to an 'x'
+    jQuery('body').toggleClass('nav-open')
+      .find('.hamburger').toggleClass('is-active');
+  });
 }
 
+
+// while (menuHeight > (windowHeight)/2) {
+//   fontSize = fontSize - 0.1;
+// }
+// for (fontSize = 2.3; menuHeight > windowHeight/2; fontSize - 0.1) {
+//
+// }
 
 // First crack at doing what sidebar-menu.js and nav.js do
 // but in vanilla JS
